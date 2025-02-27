bd43bb86117fadcbd5d4e48474e49de59cbb5735
blaneya <andy.blaney@snowflake.com>
2025-02-28 09:52:07 +1300

    changes

--- migrations/databases/raw/SOURCE_A/TABLES/R__3_TEST_TABLE.sql
+++ migrations/databases/raw/SOURCE_A/TABLES/R__3_TEST_TABLE.sql
@@ -7,3 +7,16 @@ CREATE OR ALTER TABLE TEST_TABLE
     NAME STRING,
     TS TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
 );
+
+
+{% set tbl_dmf = 
+[   {   "database": "RAW",
+        "schema": "SOURCE_A",
+        "table": "TEST_TABLE",
+        {
+            "column": "ID",
+            "dmf": "NULL_COUNT"
+        }
+    } 
+]
+%}
